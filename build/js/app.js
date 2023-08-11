439b28802ac3bea178cfae87a0e470f5962e5986
Lamine Kane <mlaminekane01@gmail.com>
2023-08-11 04:36:52 +0000

    🧑‍💻

--- build/js/app.js
+++ build/js/app.js
@@ -11,14 +11,14 @@ const img = document.getElementById('lamine-img');
 const observerOptions = {
   root: null,
   rootMargin: '0px',
-  threshold: 0.5 // Déclencher l'animation lorsque 50% de la section est visible
+  threshold: 0.5 
 };
 
 const observer = new IntersectionObserver((entries, observer) => {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
-      img.classList.add('animation-img'); // Ajoute la classe d'animation
-      observer.unobserve(entry.target); // Arrête d'observer une fois que l'animation a été déclenchée
+      img.classList.add('animation-img');
+      observer.unobserve(entry.target);
     }
   });
 }, observerOptions);
