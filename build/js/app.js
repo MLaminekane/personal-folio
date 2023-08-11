0281fbc6ec02b33dfd3f4275f72c5286675644bd
Lamine Kane <mlaminekane01@gmail.com>
2023-08-11 03:55:31 +0000

    🧑‍💻

--- build/js/app.js
+++ build/js/app.js
@@ -6,3 +6,24 @@ arrowBtn.addEventListener('click', function (e) {
     block: 'start'
   });
 });
+
+const img = document.getElementById('img1');
+const skillsSection = document.getElementById('skills');
+
+const observerOptions = {
+  root: null,
+  rootMargin: '0px',
+  threshold: 0.5 // Déclencher l'animation lorsque 50% de la section est visible
+};
+
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      img.classList.add('animation-img'); // Ajoute la classe d'animation à l'image
+      skillsSection.classList.add('animation-skills'); // Ajoute la classe d'animation aux compétences
+      observer.unobserve(entry.target); // Arrête d'observer une fois que l'animation a été déclenchée
+    }
+  });
+}, observerOptions);
+
+observer.observe(document.getElementById('about-skills'));
