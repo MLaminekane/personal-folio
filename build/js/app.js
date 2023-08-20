a44026ac199ec67a713c97d5189da6a3068f847f
Lamine Kane <mlaminekane01@gmail.com>
2023-08-20 18:42:50 +0000

    🧑‍💻

--- build/js/app.js
+++ build/js/app.js
@@ -22,5 +22,56 @@ const observer = new IntersectionObserver((entries, observer) => {
     }
   });
 }, observerOptions);
-
 observer.observe(document.getElementById('about-skills'));
+
+
+let changeColor = document.getElementById('back-color');
+let isDarkMode = false;
+changeColor.addEventListener('click', () => {
+  isDarkMode = !isDarkMode;
+
+  document.body.style.backgroundColor = isDarkMode ? 'black' : ''; 
+
+  let projects = document.querySelector("#projects");
+  projects.style.backgroundColor = isDarkMode ? 'black' : (isDarkMode ? '' : '');
+  projects.style.color = isDarkMode ? 'white' : 'black'; 
+
+  let projectBoxes = document.querySelectorAll("#project-no-page");
+  projectBoxes.forEach(project => {
+    project.style.backgroundColor = isDarkMode ? 'black' : '';
+  });
+   
+  let shadow = document.querySelectorAll("#shadow-color")
+  shadow.forEach(project =>{
+    project.style.backgroundColor = isDarkMode ? 'black' : '';
+  })
+
+  let aboutSkills = document.querySelector("#about-skills");
+  aboutSkills.style.backgroundColor = isDarkMode ? 'black' : (isDarkMode ? '' : '#141C28');
+  aboutSkills.style.color = isDarkMode ? 'white' : 'black'; 
+
+  let contact = document.querySelector("#contact");
+  contact.style.backgroundColor = isDarkMode ? 'black' : (isDarkMode ? '' : '#141C28');
+  contact.style.color = isDarkMode ? 'white' : 'black'; 
+
+  let subtitleP = document.querySelector("#junior");
+  subtitleP.style.color = isDarkMode ? 'white' : '#141C28'; 
+
+  let projectTitlesBox = document.querySelectorAll("#projects h2");
+  projectTitlesBox.forEach(title => {
+    title.style.color = isDarkMode ? 'white' : '#141C28'; 
+  });
+    let projectTitles = document.querySelectorAll("#projects h1");
+  projectTitles.forEach(title => {
+    title.style.color = isDarkMode ? 'white' : '#141C28'; 
+  });
+  
+  let titleH1 = document.querySelector("#name");
+  titleH1.style.color = isDarkMode ? 'white' : '#141C28'; 
+
+
+  let moonIcon = changeColor.querySelector('.fa-moon');
+  let sunIcon = changeColor.querySelector('.fa-sun');
+  moonIcon.style.display = isDarkMode ? 'none' : 'inline';
+  sunIcon.style.display = isDarkMode ? 'inline' : 'none';
+});
